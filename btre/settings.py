92ea57a4f6d510cc9307d03e4a5419574a6881a6
Toby <tobcvb@gmail.com>
2018-12-01 21:42:24 -0600

    Added local settings import

--- btre/settings.py
+++ btre/settings.py
@@ -148,3 +148,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER='tester7456@gmail.com'
 EMAIL_HOST_PASSWORD='drsweeper'
 EMAIL_USE_TLS=True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
